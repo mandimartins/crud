b9a593f9c47e0ed30cf5ec0db6e6ff974eb53921
mandimartins <amandamartins23@gmail.com>
2020-02-26 18:48:57 -0300

    Add sql script

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 
-const connection = require("knex")({
-  client: "mysql2",
+const connection = require('knex')({
+  client: 'mysql2',
   connection: {
-    host: "localhost",
-    user: "root",
-    password: "amds121097",
-    database: "cadastro"
+    host: 'localhost',
+    user: 'root',
+    password: 'toor',
+    database: 'cadastro'
   }
 });
 
@@ -18,22 +18,21 @@ const dependencies = {
   connection: connection
 };
 
-const pessoas = require("./routes/pessoas");
+const pessoas = require('./routes/pessoas');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // view engine
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.render("home");
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.use("/pessoas", pessoas(dependencies));
+app.use('/pessoas', pessoas(dependencies));
 
-  app.listen(port, () => console.log("CRUD listening on port" + port));
-  
+app.listen(port, () => console.log('CRUD listening on port ' + port));
